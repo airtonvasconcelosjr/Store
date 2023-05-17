0a863c5cd873c41941e22651d391dec16999514b
airtonvasconcelosjr <airton.vasconcelosjr@gmail.com>
2023-05-17 17:22:10 -0300

    Changes 1705

--- listar.php
+++ listar.php
@@ -2,25 +2,51 @@
     session_start();
     require_once("servidor.php");
 
-    if(isset($_GET['enviar'])) {
-        if(!empty($_GET['nomeproduto']) || !empty($_GET['preco']) || !empty($_GET['codigo'])) {
-            $nome=$_GET['nomeproduto'];
-            $preco=$_GET['preco'];
-            $codigo=$_GET['codigo'];
-            $extrafield=$_GET['extrafield'];
+    if (isset($_GET['enviar'])) {
+        if (!empty($_GET['nomeproduto']) || !empty($_GET['preco']) || !empty($_GET['codigo'])) {
+            $nome = $_GET['nomeproduto'];
+            $preco = $_GET['preco'];
+            $codigo = $_GET['codigo'];
+            $extrafield = $_GET['extrafield'];
 
-            $comando="INSERT INTO produtos(nome, preco, codigo, extrafield) VALUES ('$nome', '$preco', '$codigo', '$extrafield')";
-            $enviar=mysqli_query($conn, $comando);
+            if (isset($_GET['tipo'])) {
+                $tipo = $_GET['tipo'];
+                
+                if ($tipo === "Forniture") {
+                    if (!empty($_GET['altura'])) {
+                        $altura = $_GET['altura'];
+                    } else {
+                        $altura = ""; 
+                    }
 
-            if($enviar) {
-                header("location:index.php");
-                exit;
-            }else{
-                header("location:index.php");
-                exit;
+                    if (!empty($_GET['largura'])) {
+                        $largura = $_GET['largura'];
+                    } else {
+                        $largura = ""; 
+                    }
+
+                    if (!empty($_GET['comprimento'])) {
+                        $comprimento = $_GET['comprimento'];
+                    } else {
+                        $comprimento = ""; 
+                    }
+                } else {
+                    $altura = ""; 
+                    $largura = ""; 
+                    $comprimento = ""; 
+                }
+
+                $comando = "INSERT INTO produtos (nome, preco, codigo, tipo, extrafield, altura, largura, comprimento) VALUES ('$nome', '$preco', '$codigo', '$tipo', '$extrafield', '$altura', '$largura', '$comprimento')";
+                $enviar = mysqli_query($conn, $comando);
+
+                if ($enviar) {
+                    header("location:index.php");
+                    exit;
+                } else {
+                    header("location:index.php");
+                    exit;
+                }
             }
-            
         }
     }
-
 ?>
